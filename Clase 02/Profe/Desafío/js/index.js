22e92875c743e12d627e439e9979c12f578d69f6
Juan <giraldezblack@gmail.com>
2018-02-17 15:56:47 -0300

    .

---
+++ Clase 02/Profe/Desafío/js/index.js
@@ -0,0 +1,13 @@
+// Dia de la semana si/no fin de semana
+var semana = ['Lunes','Martes','Miercoles','Jueves','Viernes','Sabado','Domingo']
+var diaSemana = prompt('Ingresa una dia de la semana')
+switch (diaSemana) {
+	case 'Sabado':
+		console.log('Es fin de semana')
+		break
+	case 'Domingo':
+		console.log('Es fin de semana')
+		break
+}	
+
+
