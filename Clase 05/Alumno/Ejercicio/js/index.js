696aa697c27895fa601686f2ba35a441a22626ba
Juan <giraldezblack@gmail.com>
2018-03-03 12:24:53 -0300

    Ejercicio en clase 5

---
+++ Clase 05/Alumno/Ejercicio/js/index.js
@@ -0,0 +1,37 @@
+
+
+function Student (firstName, lastName, age, address) {
+	var id = Math.random()
+	var showfullName = firstName + ' ' + lastName
+	var modified = false
+
+	this.isLegalAge = function () {
+		if (age >= 18) {
+			return true
+		} else {
+			return false
+		}
+	}
+
+	this.setfirstName =  function (newFirstName) {
+		firstName = newFirstName
+		modified = true
+	}
+
+	this.setlastName = function (newLastName) {
+		lastName = newLastName
+		modified = true
+	}
+
+	this.getShowFullName = function () {
+		return showfullName
+	}
+
+	this.getAddress = function () {
+		return address
+	}
+
+}
+
+var newStudent = new Student('Laura','Lopez','21','Cucha cucha')
+console.log(newStudent.getShowFullName(),newStudent.isLegalAge(),newStudent.getAddress())
