5faf2cb4cec62195b18db41b128473b19ac8f387
Juan <giraldezblack@gmail.com>
2018-03-03 00:44:49 -0300

    desafio3

--- Clase 03/Alumno/Desafío/js/index.js
+++ Clase 03/Alumno/Desafío/js/index.js
@@ -1,3 +1,5 @@
+// DESAFIO 1
+
 var n1 = prompt('dame un numero!')
 var n2 = prompt('dame otro!')
 
@@ -36,3 +38,37 @@ sumNum(numero1,numero2)
 minNum(numero1,numero2)
 mulNum(numero1,numero2)
 divNum(numero1,numero2)
+
+// DESAFIO 2
+
+var operacion = prompt('que operacion queres realizar?')
+var numero1Op = toInt(prompt('dame un numero para la operacion!'))
+var numero2Op = toInt(prompt('dame otro para la operacion!'))
+
+function opFun(op) {
+
+	switch (op) {
+		case 'división':
+		case 'division':
+			do {
+				var checknumero2Op = toInt(prompt('por favor ingresa un numero distinto de 0'))
+			} while (numero2Op = 0)
+			divNum(numero1Op,checknumero2Op)
+			break
+		case 'multiplicación':
+		case 'multiplicacion':
+			mulNum(numero1Op,numero2Op)
+			break
+		case 'resta':
+			minNum(numero1Op,numero2Op)
+			break
+		case 'suma':
+			sumNum(numeroOp1,numeroOp2)
+			break
+		default :
+			console.log('por favor ingresa una operacion valida')
+	
+	}	
+
+}
+opFun(operacion.toLowerCase())
