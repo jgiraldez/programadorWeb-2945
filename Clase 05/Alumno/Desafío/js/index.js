4b9cd5f01340298727aab76bf94ee127ab209759
Juan <giraldezblack@gmail.com>
2018-03-03 15:23:50 -0300

    Incluyendo push array

--- Clase 05/Alumno/Desafío/js/index.js
+++ Clase 05/Alumno/Desafío/js/index.js
@@ -1,51 +1,62 @@
-function Student (title, year, director, actors) {
-	var id = Math.random()
-	var modified = false
-//// setters
-	this.setfirstName =  function (newFirstName) {
-		firstName = newFirstName
-		modified = true
-	}
+function Student(title, year, director, actors) {
+  var id = Math.random()
+  var modified = false
+  //// setters
+  this.setfirstName = function(newFirstName) {
+    firstName = newFirstName
+    modified = true
+  }
 
-	this.setlastName = function (newLastName) {
-		lastName = newLastName
-		modified = true
-	}
+  this.setlastName = function(newLastName) {
+    lastName = newLastName
+    modified = true
+  }
 
-	this.setDirector = function (newDirector) {
-		director = newDirector
-		modified = true
-	}
+  this.setDirector = function(newDirector) {
+    director = newDirector
+    modified = true
+  }
 
-	this.setActor = function (newActor) {
-		actors.push(newActor)
-		modified = true
-	}
-//// getters
-	this.getTitle = function () {
-		return title
-	}
+  this.setActor = function(newActor) {
+    pelicula.push({
+      actors: newActor
+    })
+    modified = true
+  }
+  //// getters
+  this.getTitle = function() {
+    return title
+  }
 
-	this.getYear = function () {
-		return year
-	}
+  this.getYear = function() {
+    return year
+  }
 
-	this.getDirector = function () {
-		return director
-	}
+  this.getDirector = function() {
+    return director
+  }
 
-	this.getActors = function () {
-		return actors
-	}
-
+  this.getActors = function() {
+    return actors
+  }
 }
-
 
 var pelicula = {
-	title: 'Bastardos sin Gloria',
-	year: 2009,
-	director: 'Quentin Tarantino',
-	actors: ['Cristoph Waltz']
+  title: 'Bastardos sin Gloria',
+  year: 2009,
+  director: 'Quentin Tarantino',
+  actors: ['Cristoph Waltz']
 }
 
-var newStudent = new Student(pelicula.title,pelicula.year,pelicula.director, pelicula.actors)
+var newStudent = new Student(
+  pelicula.title,
+  pelicula.year,
+  pelicula.director,
+  pelicula.actors
+)
+console.log(
+  newStudent.getTitle(),
+  newStudent.getDirector(),
+  newStudent.getYear(),
+  newStudent.getActors()
+)
---
+++ Clase 06/Alumno/Ejercicio/js/index.js
@@ -0,0 +1,7 @@
+var names = ['Adrian','Javier','Ines']
+
+if (Array.isArray(names)) {
+    console.log ('Es array')
+} else {
+    console.log ('No es array')
+}
